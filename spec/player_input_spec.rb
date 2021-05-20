f61df5c71631b3c4fa26370675e484b447b7762d
Ted Staros <tstaros23@gmail.com>
2021-05-20 16:18:05 -0600

    Create player input test

--- spec/player_input_spec.rb
+++ spec/player_input_spec.rb
@@ -3,12 +3,13 @@ require './lib/player_input'
 
 RSpec.describe 'PlayerInput' do
   before do
-    @player_input = PlayerInput.new
+    @player_input = gets.chomp
+    @input = PlayerInput.new(@player_input)
   #   #@player_input is now universal and can be used in other tests in the same row
   end
 
   it 'exists' do
-    expect(@player_input).to be_a(PlayerInput)
+    expect(@input).to be_a(PlayerInput)
   end
 # "BEFORE DO" executes code before other tests if they are in the same row
 
