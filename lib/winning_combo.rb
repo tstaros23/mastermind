09125bc97c430bce6356a45b093d3c4cb6fbd1dc
Ted Staros <tstaros23@gmail.com>
2021-05-19 19:14:59 -0600

    Fix  Repository

---
+++ lib/winning_combo.rb
@@ -0,0 +1,18 @@
+class WinningCombo
+  attr_reader :answer
+  def initialize
+    @answer = winning_combo
+  end
+
+  def winning_combo
+    starting_colors = ["R", "G", "B", "Y"]
+
+      winning_combo = []
+      4.times do
+        winning_combo << starting_colors.shufflle[0]
+      end
+
+      winning_combo
+    end
+
+end
