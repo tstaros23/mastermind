91f7f7f179e40c97a9648f0d5ec7b02ef4dd02db
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-20 22:05:29 -0600

    Start pseudocoding structure

---
+++ mastermind.rb
@@ -0,0 +1,18 @@
+print "Start message"
+
+input
+
+# if p, start game message, ask for input
+#  if run player_input(input)
+#  if right answer
+#    congratulations message
+#   elsif wrong answer
+#     another message
+#     run player_input again, loop to line 7
+# elsif i
+#   return instructions
+# elsif q
+#   quits
+# else
+#   loops back to beginning
+# end
