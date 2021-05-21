165d1aefde1243948f0011a39d6fed227976f489
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-20 22:07:17 -0600

    add extra tests

--- spec/player_input_spec.rb
+++ spec/player_input_spec.rb
@@ -3,17 +3,16 @@ require './lib/player_input'
 
 RSpec.describe 'PlayerInput' do
   before do
-    @player_input = gets.chomp
-    @input = PlayerInput.new(@player_input)
-  #   #@player_input is now universal and can be used in other tests in the same row
+    @input = PlayerInput.new(gets.chomp)
+    #@input is now universal and can be used in other tests in the same row
   end
 
   it 'exists' do
     expect(@input).to be_a(PlayerInput)
   end
+
+  it 'has input inside PlayerInput class' do
+    expect(@input.input).to eq(["R", "R", "R", "B"])
+  end
 # "BEFORE DO" executes code before other tests if they are in the same row
-
-
-
-
 end
