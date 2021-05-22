c540ba62eb7c3eace502cf01106c8d7948d55e63
Ted Staros <tstaros23@gmail.com>
2021-05-22 11:06:18 -0600

    Add comments to winning_combo spec

--- spec/winning_combo_spec.rb
+++ spec/winning_combo_spec.rb
@@ -1,10 +1,18 @@
 require 'rspec'
+#rspec comes from the gems
 require './lib/winning_combo'
+#from (have to be in mastermind directory) mastermind, go to the lib folder and then go to the file winning_combo
+#Don't need .rb at the end because Ruby knows what files it wants
 
 RSpec.describe 'WinningCombo' do
   before do
     @answer = WinningCombo.new
+    #this @answer is used only in this class but "before do" creates it once and is accessable throughout
+    #the whole class
     allow(@answer).to receive(:answer) {["R", "R", "B", "G"]}
+    #the winning combo class has method called answer. Answer is unpredictable because it is randomized.
+    #because answer is random, we want it to be the same each time for testing purposes
+    # we are going to mock the method answer and stub it with this value
   end
   it 'exists' do
     expect(@answer).to be_a(WinningCombo)
