f86390f5df4497df21149b447b8e869d36db54b4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-23 19:39:50 -0600

    make compare color, compare position method

--- lib/compare_color.rb
+++ lib/compare_color.rb
@@ -2,22 +2,25 @@ require './lib/winning_combo'
 require './lib/player_input'
 
 class CompareColor
-  attr_reader :players_answer, :winning_answer
+  attr_reader :players_answer, :winning_answer,
+              :correct_colors, :correct_positions
   def initialize(players_answer, winning_answer)
-    @players_answer = players_answer
-    @winning_answer = winning_answer
+    @players_answer = players_answer.input
+    @winning_answer = winning_answer.answer
+    @correct_colors = 0
+    @correct_positions = 0
+    correct_colors_positions
   end
 
-  def correct_colors
-    correct_colors = 0
-
-    @players_answer.each do |color|
-      
-
-    correct_colors
-    #saves the amount of correct colors from the iteration in an integer
-
+  def correct_colors_positions
+    @players_answer.each_with_index do |color, index|
+      if @winning_answer.include? color
+        @correct_colors += 1
+        # if the position is correct, add to second count
+        if @winning_answer[index] == @players_answer[index]
+          @correct_positions += 1
+        end
+      end
+    end
   end
-
-
 end
