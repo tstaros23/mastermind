35799d9386b961714b45b0ebc157a1d68f44e4cd
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-25 17:25:10 -0600

    refactor methods to make shorter

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,3 @@
-require 'rspec'
 require './lib/statement'
 require './lib/player_input'
 require './lib/compare_color'
@@ -8,27 +7,40 @@ require './lib/winning_combo'
 class Game
   def initialize
     @statement = Statement.new
+    @guesses = 0
   end
   #game.new makes Statement.new available in the game class
   def main_menu_interaction
+    mastermind_intro
+    if @statement.input == "P"
+    elsif @statement.input == "I"
+      instructions
+    elsif @statement.input == "Q"
+    else
+      wrong_input
+    end
+  end
+
+  private
+
+  def instructions
+    system "clear"
+    @statement.print_to_terminal(@statement.instructions)
+    @statement.print_to_terminal(@statement.get_player_input)
+    return main_menu_interaction if @statement.input != nil
+  end
+
+  def mastermind_intro
     system "clear"
     @statement.print_to_terminal(@statement.mastermind_art)
     @statement.print_to_terminal(@statement.main_menu)
     @statement.print_to_terminal(@statement.get_player_input)
   #making the Statement class an instance variable within the Game class will let us use the methods that come with the Statement class that are outside of the global scope
-    if @statement.input == "P"
-    elsif @statement.input == "I"
-      @statement.print_to_terminal(@statement.instructions)
-      @statement.print_to_terminal(@statement.get_player_input)
-      if @statement.input == "Q"
-      elsif @statement.input != nil
-        main_menu_interaction
-      end
-    elsif @statement.input == "Q"
-    else
-      @statement.print_to_terminal(@statement.wrong_input)
-      sleep 2
-      main_menu_interaction
-    end
+  end
+
+  def wrong_input
+    @statement.print_to_terminal(@statement.wrong_input)
+    sleep 2
+    main_menu_interaction
   end
 end
